885cc98a9090110008b5bfa5388e4aaf97eb6c64
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-04 14:36:30 -0400

    Import 'Header', 'SearchBar' in 'Home'.

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
+import Header from '../components/Header';
+import SearchBar from '../components/SearchBar';
 
 const Home = () => {
 	return (
 		<View>
 			{/* Header */}
+			<Header />
 			{/* Search Bar */}
+			<SearchBar />
 			{/* Menu Buttons */}
 			{/* Contacts Menu */}
 		</View>
