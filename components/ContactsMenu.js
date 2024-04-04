a6ea6dbc173a666bfcf226630fb5f40b1f2d54d6
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-04 14:51:12 -0400

    Create 'MenuButtons', 'ContactsMenu'.

---
+++ components/ContactsMenu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ContactsMenu = () => {
+	return (
+		<View>
+			<Text>ContactsMenu</Text>
+		</View>
+	);
+};
+
+export default ContactsMenu;
---
+++ components/MenuButtons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MenuButtons = () => {
+	return (
+		<View>
+			<Text>MenuButtons</Text>
+		</View>
+	);
+};
+
+export default MenuButtons;
