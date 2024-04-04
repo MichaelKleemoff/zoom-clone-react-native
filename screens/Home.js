645d0ee7e417420513dcb547feeda0d85c116daa
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-04 15:04:58 -0400

    Import and implement 'StyleSheet' in 'Home'.

--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import { View, SafeAreaView } from 'react-native';
+import { View, SafeAreaView, StyleSheet } from 'react-native';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
+import MenuButtons from '../components/MenuButtons';
+import ContactsMenu from '../components/ContactsMenu';
 
 const Home = () => {
 	return (
-		<View>
+		<View style={styles.container}>
 			<SafeAreaView>
 				{/* Header */}
 				<Header />
 				{/* Search Bar */}
 				<SearchBar />
 				{/* Menu Buttons */}
+				<MenuButtons />
 				{/* Contacts Menu */}
+				<ContactsMenu />
 			</SafeAreaView>
 		</View>
 	);
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+	container: {},
+});
