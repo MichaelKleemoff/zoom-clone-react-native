2381495c2e3ca6201b320b4a7e4983ab9f9e68ed
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-04 14:17:12 -0400

    Create 'components' folder, 'Header.js','SearcBar.js'.

---
+++ components/SearchBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const SearchBar = () => {
+	return <div>SearchBar</div>;
+};
+
+export default SearchBar;
--- screens/Home.js
+++ screens/Home.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { View } from 'react-native';
 
 const Home = () => {
-	return <View></View>;
+	return (
+		<View>
+			{/* Header */}
+			{/* Search Bar */}
+			{/* Menu Buttons */}
+			{/* Contacts Menu */}
+		</View>
+	);
 };
 
 export default Home;
