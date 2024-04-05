37f16a63b63117ca29e9e73736220aecfa657818
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-05 12:56:01 -0400

    Add 'notification', 'new-message' icons in 'Header'.

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,13 @@ import { View, Text, StyleSheet } from 'react-native';
 import Entypo from 'react-native-vector-icons/Entypo';
 
 const Header = () => {
-	return <View style={styles.container}></View>;
+	return (
+		<View style={styles.container}>
+			<Entypo name='notification' size={30} color='#efefef' />
+
+			<Entypo name='new-message' size={30} color='#efefef' />
+		</View>
+	);
 };
 
 export default Header;
