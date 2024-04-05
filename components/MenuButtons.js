fd76200e9f57212989c020cd9398bf1c6eb08852
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-05 16:06:42 -0400

    Start styling 'New Meeting' button in 'MenuButtons' with 'video-camera' icon from 'FontAwesome'.

--- components/MenuButtons.js
+++ components/MenuButtons.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const MenuButtons = () => {
 	return (
-		<View>
-			<Text>MenuButtons</Text>
+		<View style={styles.container}>
+			{/* One Button */}
+			<View style={styles.buttonContainer}>
+				<TouchableOpacity>
+					<FontAwesome name={'video-camera'} size={23} color={'#efefef'} />
+				</TouchableOpacity>
+			</View>
 		</View>
 	);
 };
 
 export default MenuButtons;
+
+const styles = StyleSheet.create({
+	container: {},
+	buttonContainer: {},
+});
