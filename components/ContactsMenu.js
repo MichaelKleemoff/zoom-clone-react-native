4a7b079374ccf0de836e1f9fbcb5c2819667bcfe
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-06 11:28:08 -0400

    Add 'styles' to 'ContactsMenu'.

--- components/ContactsMenu.js
+++ components/ContactsMenu.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const ContactsMenu = () => {
 	return (
-		<View>
+		<View style={styles.container}>
 			<Text>ContactsMenu</Text>
 		</View>
 	);
 };
 
 export default ContactsMenu;
+
+const styles = StyleSheet.create({
+	container: {},
+});
