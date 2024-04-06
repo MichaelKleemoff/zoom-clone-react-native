eb9658cdfa226d52dd197fb631aa1aef76ae84e2
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-06 15:05:15 -0400

    Create 'MeetingRoom.js'. Import 'createStackNavigator', 'NavigationContainer'.

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
 import Home from './screens/Home';
 
 export default function App() {
---
+++ screens/MeetingRoom.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const MeetingRoom = () => {
+	return (
+		<View>
+			<Text>Meeting Room</Text>
+		</View>
+	);
+};
+
+export default MeetingRoom;
+
+const styles = StyleSheet.create({});
