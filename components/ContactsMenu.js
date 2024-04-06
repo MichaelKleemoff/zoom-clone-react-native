2726826bacefe144d19978afb02798d6a93747c7
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-06 14:09:37 -0400

    Import 'Image'. Create buttons array, map through them and conditionally render images.

--- components/ContactsMenu.js
+++ components/ContactsMenu.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const conctactsMenuButtons = [
+	{
+		type: 'starred',
+	},
+	{
+		type: 'contact',
+		name: 'Michael Kleemoff',
+		photo: require('../assets/michael-profile-pic.jpeg'),
+	},
+];
 
 const ContactsMenu = () => {
 	return (
 		<View style={styles.container}>
 			{/* Contact Container */}
-			<View style={styles.row}>
-				{/* Image */}
-				<View style={styles.starredIcon}>
-					<AntDesign name='star' size={30} color='#efefef' />
+			{conctactsMenuButtons.map((contact, index) => (
+				<View style={styles.row} key={index}>
+					{/* Image */}
+					{contact.type === 'starred' ? (
+						<View style={styles.starredIcon}>
+							<AntDesign name='star' size={30} color='#efefef' />
+						</View>
+					) : (
+						<Image source={contact.photo} style={styles.image} />
+					)}
+					{/* Text */}
+					<Text style={styles.text}>Starred</Text>
 				</View>
-				{/* Text */}
-				<Text style={styles.text}>Starred</Text>
-			</View>
+			))}
 		</View>
 	);
 };
@@ -22,7 +39,11 @@ export default ContactsMenu;
 
 const styles = StyleSheet.create({
 	container: {},
-	text: {},
+	text: {
+		color: '#fff',
+		paddingLeft: 15,
+		fontSize: 18,
+	},
 	row: {
 		flexDirection: 'row',
 		marginTop: 20,
@@ -34,6 +55,11 @@ const styles = StyleSheet.create({
 		height: 55,
 		justifyContent: 'center',
 		alignItems: 'center',
+		borderRadius: 20,
+	},
+	image: {
+		width: 55,
+		height: 55,
 		borderRadius: 20,
 	},
 });
