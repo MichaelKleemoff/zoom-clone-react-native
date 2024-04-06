3241e05a438b1813a05377c0ed8c4d5d99db2309
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-06 12:45:27 -0400

    Import 'AntDesign' for star image and begin styling 'Starred' button.

--- components/ContactsMenu.js
+++ components/ContactsMenu.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const ContactsMenu = () => {
 	return (
 		<View style={styles.container}>
-			<Text>ContactsMenu</Text>
+			{/* Contact Container */}
+			<View style={styles.row}>
+				{/* Image */}
+				<View style={styles.starredIcon}>
+					<AntDesign name='star' size={30} color='#efefef' />
+				</View>
+				{/* Text */}
+				<Text style={styles.text}>Starred</Text>
+			</View>
 		</View>
 	);
 };
@@ -13,4 +22,18 @@ export default ContactsMenu;
 
 const styles = StyleSheet.create({
 	container: {},
+	text: {},
+	row: {
+		flexDirection: 'row',
+		marginTop: 20,
+		alignItems: 'center',
+	},
+	starredIcon: {
+		backgroundColor: '#333',
+		width: 55,
+		height: 55,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 20,
+	},
 });
