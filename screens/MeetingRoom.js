1296322a84867b07de02c7383033108a1793e260
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-08 10:14:12 -0400

    Add 2 'TextInput's and import 'useState'.

--- screens/MeetingRoom.js
+++ screens/MeetingRoom.js
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
 
 const MeetingRoom = () => {
 	return (
 		<View>
-			<Text>Meeting Room</Text>
+			<View style={styles.startMeetingContainer}>
+				<View style={styles.info}>
+					<TextInput style={styles.textInput} />
+				</View>
+				<View style={styles.info}>
+					<TextInput style={styles.textInput} />
+				</View>
+			</View>
 		</View>
 	);
 };
 
 export default MeetingRoom;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {},
+	startMeetingContainer: {},
+	info: {},
+	textInput: {},
+});
