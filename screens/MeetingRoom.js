0933c63562aa6350bc052a036dbe41149502199c
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-08 10:18:40 -0400

    Add 'name' hook and use it in 1st 'TextInput' in 'MeetingRoom'.

--- screens/MeetingRoom.js
+++ screens/MeetingRoom.js
@@ -2,11 +2,17 @@ import { StyleSheet, Text, TextInput, View } from 'react-native';
 import React, { useState } from 'react';
 
 const MeetingRoom = () => {
+	const [name, setName] = useState('');
+
 	return (
 		<View>
 			<View style={styles.startMeetingContainer}>
 				<View style={styles.info}>
-					<TextInput style={styles.textInput} />
+					<TextInput
+						style={styles.textInput}
+						value={name}
+						onChangeText={(text) => setName(text)}
+					/>
 				</View>
 				<View style={styles.info}>
 					<TextInput style={styles.textInput} />
